880bcad186909b5f03de91406c87cbfad26b868a
Subak Systems <info@subak.jp>
2020-04-06 22:28:22 +0900

    2020年 4月 6日 月曜日 22時28分22秒 JST

--- index.ts
+++ index.ts
@@ -1,20 +1,23 @@
-import {VNode, VNodeData as VNodeDataOrigin} from 'snabbdom/vnode'
+import {VNode} from 'snabbdom/vnode'
 import classModule from 'snabbdom/modules/class'
 import propsModule from 'snabbdom/modules/props'
 import attributesModule from 'snabbdom/modules/attributes'
 import styleModule from 'snabbdom/modules/style'
 import eventListenersModule from "snabbdom/modules/eventlisteners"
+import datasetModule from "snabbdom/modules/dataset";
+import jsonModule from './modules/json';
 import {merge} from 'rxjs'
 import {source, sink, Socket, LifecyclePort} from 'pkit/core'
 import {renderSink, terminateSink} from './processors'
-import {ActionVNodeData} from './action';
 
 export const defaultModules = [
   classModule,
   propsModule,
   attributesModule,
   styleModule,
-  eventListenersModule];
+  eventListenersModule,
+  jsonModule,
+  datasetModule];
 
 export class SnabbdomPort extends LifecyclePort {
   render = new Socket<VNode>();
@@ -29,7 +32,6 @@ export const useSnabbdom = (port: SnabbdomPort, lifecycle: LifecyclePort, contai
     terminateSink(container)(source(lifecycle.terminate), sink(port.terminated),
       [source(port.rendered)]));
 
-export interface VNodeData extends VNodeDataOrigin, ActionVNodeData {}
 
 export {default as action, ActionData} from './action'
 export {default as h} from './h'
