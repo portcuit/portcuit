a35df36fc6267bf8cf5af1b316d27d778cfef837
Subak Systems <info@subak.jp>
2020-04-10 15:16:12 +0900

    createLatestMapProc <- createLatestMapSink

--- processors.ts
+++ processors.ts
@@ -5,18 +5,12 @@ import {toVNode} from 'snabbdom/tovnode'
 import {Observable} from 'rxjs'
 import {map, scan} from 'rxjs/operators'
 import {Sink} from 'pkit/core'
-// import {BindProcessor, WithBindProcessor1, withLatestMapSink} from 'pkit1/processors'
-import {createLatestMapSink} from 'pkit/processors'
+import {createLatestMapProc} from 'pkit/processors'
 
 export const terminateSink = (container: HTMLElement) =>
-  createLatestMapSink<unknown, Node, VNode>(
+  createLatestMapProc<unknown, Node, [VNode]>(
     ([,vnode]) =>
       vnode.elm.parentNode.replaceChild(container, vnode.elm));
-
-// export const terminate: WithBindProcessor1<[PortData,[VNode]],[Node],[HTMLElement]> = (source$, sink, sources$, [container]) =>
-//   withLatestMapSink(source$, sink, sources$,
-//     ([,[vnode]]) =>
-//       vnode.elm.parentNode.replaceChild(container, vnode.elm));
 
 export const renderSink = (container: HTMLElement, modules: Module[]) => {
   const patch = init(modules);
@@ -28,13 +22,3 @@ export const renderSink = (container: HTMLElement, modules: Module[]) => {
       map(value =>
         sink(value)));
 };
-
-// export const render: BindProcessor<[VNode], [VNode], [HTMLElement, Module[]]> = (source$, sink, [container, modules]) => {
-//   const patch = init(modules);
-//   return source$.pipe(
-//     scan(
-//       (acc, [vnode]) =>
-//         patch(acc || toVNode(container), vnode), null),
-//     map(value =>
-//       sink(value)))
-// };
