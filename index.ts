d31b14a7096c556291faac736ec5acc88d3ac881
Subak Systems <subak@rmb12.local>
2020-08-20 15:14:05 +0900

    2020年 8月20日 木曜日 15時14分05秒 JST

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
-import type {Module} from "snabbdom/modules/module";
 import type {VNode} from 'snabbdom/vnode'
+import {fromEvent, merge} from 'rxjs'
+import {map, scan, switchMap, filter} from "rxjs/operators";
 import {init} from 'snabbdom/init'
 import {classModule} from 'snabbdom/modules/class'
 import {propsModule} from 'snabbdom/modules/props'
@@ -8,13 +9,14 @@ import {styleModule} from 'snabbdom/modules/style'
 import {eventListenersModule} from "snabbdom/modules/eventlisteners"
 import {datasetModule} from "snabbdom/modules/dataset";
 import {toVNode} from "snabbdom/tovnode";
-import {merge} from 'rxjs'
-import {map, scan, switchMap} from "rxjs/operators";
-import {source, sink, Socket, LifecyclePort} from 'pkit/core'
-import {latestMapProc} from "pkit/processors";
+import {source, sink, Socket, LifecyclePort, DeepPartial} from 'pkit/core'
+import {directProc, latestMapProc, mergeMapProc} from "pkit/processors";
 import {selectorModule} from './modules/selector';
 import {triggerModule} from './modules/trigger'
 import {jsxModule} from './modules/jsx'
+import {createActionModule} from "./modules/action";
+import {ActionDetail, actionProc} from "./index";
+import {StatePort} from "pkit/state";
 
 export * from './modules/action'
 export * from './processors'
@@ -33,30 +35,55 @@ export const defaultModules = [
 
 export type SnabbdomParams = {
   container: Element;
-  modules?: Module[];
+  target: EventTarget;
+  options?: {
+    window: Window;
+    hashchange?: boolean
+  }
 }
 
 export class SnabbdomPort extends LifecyclePort<SnabbdomParams> {
   render = new Socket<VNode>();
   vnode = new Socket<VNode>();
+  action = new Socket<ActionDetail>();
+  event = new class {
+    hashchange = new Socket<string>();
+  }
 }
 
 export const snabbdomKit = (port: SnabbdomPort) =>
   merge(
     source(port.init).pipe(
-      switchMap(({container, modules = defaultModules}) => {
-        const patch = init(modules);
-        return source(port.render).pipe(
+      switchMap(({container, target}) => {
+        const patch = init([createActionModule(target), ...defaultModules]);
+        return directProc(source(port.render).pipe(
           scan((acc, vnode) =>
-            patch(acc, vnode), toVNode(container)),
-          map((value) =>
-            sink(port.vnode)(value))
-        )
+            patch(acc, vnode), toVNode(container))),
+          sink(port.vnode));
       })),
     latestMapProc(source(port.terminate), sink(port.info), [source(port.init), source(port.vnode)] as const,
       ([,{container}, vnode]) =>
-        vnode!.elm!.parentNode!.replaceChild(container, vnode!.elm!)
-    )
+        vnode!.elm!.parentNode!.replaceChild(container, vnode!.elm!)),
+    mergeMapProc(source(port.init), sink(port.dom.action), ({target}) =>
+      fromEvent<CustomEvent<ActionDetail>>(target as any, 'action').pipe(
+        map(({detail}) =>
+          detail))),
+    optionsKit(port)
   );
 
+export const snabbdomActionPatchKit = <T>(port: SnabbdomPort, state: StatePort<T>) =>
+  actionProc(source(port.action), sink(state.patch))
 
+const optionsKit = (port: SnabbdomPort) =>
+  source(port.init).pipe(
+    filter(({options}) =>
+      !!options),
+    map(({options}) =>
+      options!),
+    switchMap(({window, hashchange}) =>
+      merge(...(hashchange ? [
+        directProc(fromEvent<void>(window, 'hashchange').pipe(
+          map(() =>
+            window.location.hash)), sink(port.dom.event.hashchange))
+      ]: [])))
+  )
