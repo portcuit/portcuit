51bf234ce101f74ddc2288a10fffeeff16d6703d
Subak Systems <subak@rmbp13.local>
2020-09-03 17:43:04 +0900

    Thu Sep  3 17:43:03 JST 2020

--- csr/processors.ts
+++ csr/processors.ts
@@ -6,8 +6,8 @@ import {Action, ActionDetail} from "./modules/action";
 export const actionProc = <T>(source$: Observable<ActionDetail>, sink: Sink<DeepPartial<T>>) =>
   source$.pipe(
     map(([fn, data]) =>
-      sink(new Function(`return ({ReplaceObject, ReplaceArray, EphemeralBoolean, splice, padArray}) => ${fn};`)()
-      ({ReplaceObject, ReplaceArray, EphemeralBoolean, splice, padArray})(data))))
+      sink(new Function(`return ({ReplaceObject, ReplaceArray, EphemeralBoolean, splice, padArray, pkit}) => ${fn};`)()
+      ({ReplaceObject, ReplaceArray, EphemeralBoolean, splice, padArray, pkit: {ReplaceObject, ReplaceArray, EphemeralBoolean, splice, padArray}})(data))))
 
 export const action = <T, U = undefined>(action: Action<T,U>, detail?: U) =>
   [Object.entries(action).reduce((acc, [key, value]) =>
