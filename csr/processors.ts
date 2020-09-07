052b02ab83dab2a2b0fef0a95cf9c64ae93e45e8
Subak Systems <subak@rmbp13.local>
2020-09-07 13:30:24 +0900

    Mon Sep  7 13:30:24 JST 2020

--- csr/processors.ts
+++ csr/processors.ts
@@ -1,14 +1,14 @@
 import {Observable} from "rxjs";
 import {map} from "rxjs/operators";
-import {DeepPartial, Sink, ReplaceObject, ReplaceArray, EphemeralBoolean, EphemeralContainer, splice, padArray} from "pkit";
+import {DeepPartial, Sink, ReplaceObject, ReplaceArray, EphemeralBoolean, EphemeralString, EphemeralContainer, splice, padArray} from "pkit";
 import {Action, ActionDetail} from "./modules/action";
 
-const pkit = {ReplaceObject, ReplaceArray, EphemeralBoolean, EphemeralContainer, splice, padArray}
+const pkit = {ReplaceObject, ReplaceArray, EphemeralBoolean, EphemeralString, EphemeralContainer, splice, padArray}
 
 export const actionProc = <T>(source$: Observable<ActionDetail>, sink: Sink<DeepPartial<T>>) =>
   source$.pipe(
     map(([fn, data]) =>
-      sink(new Function(`return ({ReplaceObject, ReplaceArray, EphemeralBoolean, EphemeralContainer, splice, padArray, pkit}) => ${fn};`)()
+      sink(new Function(`return ({ReplaceObject, ReplaceArray, EphemeralBoolean, EphemeralString, EphemeralContainer, splice, padArray, pkit}) => ${fn};`)()
       ({...pkit, pkit})(data))))
 
 export const action = <T, U = undefined>(action: Action<T,U>, detail?: U) =>
