b189aeaa86ca1808068a56d634729a2d56a08bbc
Subak Systems <subak@rmbp13.local>
2020-09-09 01:11:53 +0900

    Wed Sep  9 01:11:53 JST 2020

--- ssr/index.ts
+++ ssr/index.ts
@@ -1,1 +1,25 @@
+import {VNode} from "snabbdom/vnode";
 import 'snabbdom-to-html'
+import init from 'snabbdom-to-html/init'
+import {merge} from "rxjs";
+import {latestMapProc, LifecyclePort, sink, Socket, source} from "pkit";
+
+import {jsxModule} from './modules/jsx'
+import {classNamesModule} from './modules/classNames'
+
+const toHTML = init([jsxModule, classNamesModule]);
+
+export type SnabbdomSsrParams = {
+  fragment: boolean
+}
+
+export class SnabbdomSsrPort<T extends SnabbdomSsrParams = SnabbdomSsrParams> extends LifecyclePort<T> {
+  render = new Socket<VNode>();
+  html = new Socket<string>();
+}
+
+export const snabbdomSsrKit = (port: SnabbdomSsrPort) =>
+  merge(
+    latestMapProc(source(port.render), sink(port.html), [source(port.init)], ([vnode, {fragment = true}]) =>
+      (fragment ? '<!DOCTYPE html>' : '') + toHTML(vnode))
+  )
