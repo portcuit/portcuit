4d967de728822ef999d2d5dc2384c2e6eb1b1733
Subak Systems <subak@rmbp13.local>
2020-09-09 15:27:58 +0900

    Wed Sep  9 15:27:58 JST 2020

--- ssr/modules/jsx.ts
+++ ssr/modules/jsx.ts
@@ -4,9 +4,14 @@ export const jsxModule: Module = (vnode, attributes) => {
   if (!vnode.data) return;
 
   for (const [key, value] of Object.entries(vnode.data)) {
-    if (['sel', 'classNames', 'bind'].includes(key)) continue;
-    if (vnode.sel === 'textarea' && key === 'value') {
+    if (['sel', 'classNames', 'bind'].includes(key)) {
+
+    } else if (vnode.sel === 'textarea' && key === 'value') {
       vnode.text = value;
+    } else if (key === 'innerHTML') {
+      vnode.data.props = {
+        innerHTML: value
+      }
     } else {
       attributes.set(key, value);
     }
