8a5d154d8319951a0966726c6f62005cf8f30545
Subak Systems <subak@rmbp13.local>
2020-09-16 17:33:17 +0900

    Wed Sep 16 17:33:17 JST 2020

--- http/server/api/index.ts
+++ http/server/api/index.ts
@@ -1,31 +1,20 @@
-import {OutgoingHttpHeaders} from "http";
 import {promisify} from 'util'
-import type {VNode} from 'snabbdom/vnode'
-import 'snabbdom-to-html'
-import init from 'snabbdom-to-html/init'
+import {OutgoingHttpHeaders} from "http";
 import {fromEvent, merge} from "rxjs";
 import {filter, reduce, takeUntil, map} from "rxjs/operators";
-import {LifecyclePort, sink, Socket, source} from "pkit/core";
-import {latestMergeMapProc, mapProc, mapToProc, mergeMapProc} from "pkit/processors";
+import {LifecyclePort, sink, Socket, source, latestMergeMapProc, mapProc, mapToProc, mergeMapProc} from "pkit";
 import {RequestArgs, isNotReserved} from "pkit/http/server/processors";
-import {selectorModule} from '@pkit/snabbdom/ssr/modules/selector'
-import {jsxModule} from '@pkit/snabbdom/ssr/modules/jsx'
-import {classNamesModule} from '@pkit/snabbdom/ssr/modules/classNames'
-
-const toHTML = init([selectorModule, jsxModule, classNamesModule]);
 
 type ApiResponse = readonly [status: number, headers: OutgoingHttpHeaders, body: any];
 
 class ContentTypePort {
   json = new Socket<any>();
   html = new Socket<string>();
-  vnode = new Socket<VNode>();
 }
 
 export class HttpServerApiPort extends LifecyclePort<RequestArgs> implements ContentTypePort {
   json = new Socket<any>();
   html = new Socket<string>();
-  vnode = new Socket<VNode>();
   notFound = new ContentTypePort;
   body = new Socket<any>();
   terminate = new Socket<ApiResponse>();
@@ -46,10 +35,6 @@ export const httpServerApiKit = (port: HttpServerApiPort) =>
         res.writeHead(statusCode, headers);
         return promisify<string>(res.end).call(res, body)
       }),
-    mapProc(source(port.vnode), sink(port.html), (data) =>
-      `<!DOCTYPE html>${toHTML(data)}`),
-    mapProc(source(port.notFound.vnode), sink(port.notFound.html), (data) =>
-      `<!DOCTYPE html>${toHTML(data)}`),
     mergeMapProc(source(port.init), sink(port.body), ([req, res]) =>
       fromEvent<Buffer>(req, 'data').pipe(
         takeUntil(fromEvent(req, 'end')),
@@ -63,4 +48,3 @@ export const httpServerApiTerminateKit = (port: HttpServerApiPort) =>
   merge(
     mapToProc(source(port.init).pipe(filter(isNotReserved)), sink(port.terminated)),
   )
-
