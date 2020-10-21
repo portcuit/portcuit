db010611676a7f4d3061a719010c694ff6b6fa5e
Subak Systems <subak@rmbp13.local>
2020-10-21 14:05:02 +0900

    Wed Oct 21 14:05:02 JST 2020

--- server/index.ts
+++ server/index.ts
@@ -8,39 +8,61 @@ import {
   sink,
   source,
   mergeMapProc,
-  entry,
   terminatedComplete,
   mapToProc,
   mount,
   LifecyclePort,
   mapProc,
-  PortMessage, Sink, EphemeralString
+  PortMessage, Sink, EphemeralString, latestMergeMapProc
 } from "pkit";
-import {HttpServerPort, route, HttpServerParams, Route} from "pkit/http/server";
+import {HttpServerPort, route, HttpServerParams, Route, HttpServerRestPort, makeHtmlResponse} from "pkit/http/server";
 import {CreateSsr} from "./ssr/";
 import {NextSsrPort} from "@pkit/next/server/index";
 import {IState} from "@pkit/next";
 import {HttpServerContext} from "pkit/http/server/index";
 import {FC} from "@pkit/snabbdom";
 
+type Logger = {
+  createLogger: (prefix: string) => () => void
+}
+
 export * from './ssr/'
 
 interface ISsrPort<T> {
   new (...args: any[]): {
-    entry: <U extends NextSsrPort.Params<T>>(params: U) => Observable<PortMessage<any>>;
+    entry: <U extends NextSsrPort.Params<T>>(params: U, logger?: ()=>void) => Observable<PortMessage<any>>;
   }
 }
 
 export const createCreatePortProc = <T extends IState, U extends ISsrPort<T>>(Port: U) =>
-  (Html: FC<T>, state: T, matchRoute: Route) =>
-    (source$: Observable<HttpServerContext>, sink: Sink<any>) =>
-      mergeMapProc(route(matchRoute.path, source$, matchRoute.method), sink, (ctx) =>
-        new Port().entry({
-          Html, ctx,
-          state: {...state,
-            flag: {method: new EphemeralString(ctx[0].method!)}
-          }
-        }))
+  (Html: FC<T>, state: T, matchRoute: Route, prefix = '/ssr/') =>
+    (source$: Observable<HttpServerContext>, sink: Sink<any>, logger$: Observable<Logger>) =>
+      latestMergeMapProc(route(matchRoute.path, source$, matchRoute.method), sink,
+        [logger$], ([ctx, {createLogger}]) =>
+          new Port().entry({
+            Html, ctx,
+            state: {...state,
+              flag: {method: new EphemeralString(ctx[0].method!)}
+            }
+        }, createLogger(prefix)))
+
+export const notFoundProc = (source$: Observable<HttpServerContext>, debugSink: Sink<any>, logger$: Observable<Logger>, prefix='/404/') =>
+  latestMergeMapProc(route('**', source$), debugSink, [logger$], ([ctx, {createLogger}]) =>
+    new HttpServerRestPort().entry((rest) =>
+      merge(
+        HttpServerRestPort.prototype.circuit(rest),
+        mapToProc(source(rest.init), sink(rest.response.raw),
+          makeHtmlResponse('Not Found', {status: 404}))
+      ), ctx, createLogger(prefix)))
+
+export const staticProc = (source$: Observable<HttpServerContext>, sink: Sink<any>, doc: {root: string, prefix?: string}) =>
+  mergeMapProc(route('**', source$), sink, async ([req, res]) => {
+    const appName = req.url!.split('/')[1];
+    if (doc.prefix && (!appName || !['src', 'node_modules'].includes(appName))) {
+      req.url = doc.prefix + req.url
+    }
+    return ({handler: await handler(req, res, {public: doc.root, cleanUrls: false})})
+  })
 
 export type NextHttpParams = {
   server: HttpServerParams;
