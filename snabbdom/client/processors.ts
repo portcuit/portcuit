e631e6208f4b69a8a8cfb8584e013aa0d27991bb
Subak Systems <subak@m1mini.local>
2021-04-04 17:01:08 +0900

    Sun Apr  4 17:01:08 JST 2021

--- snabbdom/client/processors.ts
+++ snabbdom/client/processors.ts
@@ -1,7 +1,9 @@
 import {Action} from "./modules/action";
 
 export const action = <T, U = undefined>(action: Action<T,U>, detail?: U) =>
-  [Object.entries(action).reduce((acc, [key, value]) =>
-    ({...acc,
-      [key]: value!.toString()
-    }), {}), detail]
+  [
+    Object.fromEntries(Object.entries(action)
+      .map(([key, value]) =>
+        [key, value!.toString()])),
+    detail
+  ]
