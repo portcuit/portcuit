d32ad0ae162fb5ab267587cce87d74a0d71a581d
Subak Systems <subak@m1mini.local>
2021-05-18 18:11:08 +0900

    Tue May 18 18:11:08 JST 2021

--- core/state/index.ts
+++ core/state/index.ts
@@ -113,12 +113,6 @@ export class StatePort<T extends {}> {
   }
 }
 
-type Primitive<T> =
-  T extends Boolean ? T :
-    T extends Number ? T :
-      T extends String ? T :
-        never
-
 declare const extra: unique symbol;
 
 export type PartialState<T> =
@@ -126,9 +120,24 @@ export type PartialState<T> =
     { [P in keyof T]?: PartialState<T[P]> | null; } & {[extra]?: Error}
     : T | null;
 
+type Primitive<T> =
+  T extends Boolean ? T :
+    T extends Number ? T :
+      T extends String ? T :
+        never
 
 export type PartialStateX<T> =
   T extends Primitive<T> ? T :
     T extends object ?
       { [P in keyof T]?: PartialState<T[P]>; } & {[extra]?: Error}
       : T;
+
+export const toRecord = <T extends {[key: string]: any}, U extends keyof T>(rows: Array<T>, id: U) => {
+  return rows.reduce((memo, curr) =>
+    ({
+      ...memo,
+      [curr[id]]: curr
+    }), {}) as Record<T[U], T>
+}
+
+export type StateRecord<T extends {[key: string]: any}, U extends keyof T> = Record<T[U], T>
