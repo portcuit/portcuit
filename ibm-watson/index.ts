bf592667437131384d4d63588a5ce124bd240675
Subak Systems <subak@m1mini.local>
2021-05-27 15:03:18 +0900

    Thu May 27 15:03:18 JST 2021

---
+++ ibm-watson/index.ts
@@ -0,0 +1,1 @@
+export * from './speech_to_text/'
--- ibm-watson/speech_to_text/index.ts
+++ ibm-watson/speech_to_text/index.ts
@@ -1,24 +1,45 @@
-import SpeechToTextV1 from 'ibm-watson/speech-to-text/v1'
+import SpeechToTextV1, {RecognizeWebSocketParams} from 'ibm-watson/speech-to-text/v1'
+import {SpeechRecognitionResults} from "ibm-watson/speech-to-text/v1-generated";
+import RecognizeStream from 'ibm-watson/lib/recognize-stream'
 import {IamAuthenticator} from 'ibm-watson/auth'
-import {config} from 'dotenv'
+import {cycleFlow, directProc, LifecyclePort, mapProc, sink, Socket, source} from "@pkit/core";
+import {fromEvent, merge, of} from "rxjs";
+import {switchMap} from "rxjs/operators";
 
-config();
+export class IbmWatsonSpeechToTextPort extends LifecyclePort {
+  init = new Socket<{
+    apikey: string,
+    serviceUrl: string,
+    recognizeWebSocketParams: RecognizeWebSocketParams
+  }>();
 
-const speechToText = new SpeechToTextV1({
-  authenticator: new IamAuthenticator({
-    apikey: `${process.env.IBM_WATSON_APIKEY}`,
-  }),
-  serviceUrl: `${process.env.IBM_WATSON_SPEECH_TO_TEXT_URL}`,
-  disableSslVerification: true,
-});
+  speechToText = new Socket<SpeechToTextV1>();
+  recognizeStream = new Socket<RecognizeStream>();
+  speechRecognitionResults = new Socket<SpeechRecognitionResults>();
 
-const params = {
-  objectMode: true,
-  contentType: 'audio/flac',
-  model: 'en-US_BroadbandModel',
-  keywords: ['colorado', 'tornado', 'tornadoes'],
-  keywordsThreshold: 0.5,
-  maxAlternatives: 3,
-};
+  circuit() {
+    return cycleFlow(this, 'init', 'terminated', {
+      speechToTextFlow: (port, params) =>
+        mapProc(of(params), sink(port.speechToText),
+          ({apikey, serviceUrl}) =>
+            new SpeechToTextV1({
+              authenticator: new IamAuthenticator({apikey}),
+              serviceUrl,
+              disableSslVerification: true,
+            })),
 
-const recognizeStream = speechToText.recognizeUsingWebSocket(params);
+      recognizeStreamFlow: (port, {recognizeWebSocketParams}) =>
+        mapProc(source(port.speechToText), sink(port.recognizeStream),
+          (speechToText) =>
+            speechToText.recognizeUsingWebSocket(recognizeWebSocketParams)),
+
+      streamEventFlow: (port) =>
+          source(port.recognizeStream).pipe(
+            switchMap((recognizeStream) => merge(
+              directProc(fromEvent(recognizeStream, 'data'), sink(port.speechRecognitionResults)),
+              directProc(fromEvent(recognizeStream, 'error'), sink(port.err)),
+              directProc(fromEvent(recognizeStream, 'close'), sink(port.terminated))
+            )))
+    })
+  }
+}
