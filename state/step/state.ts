27ffb54e46da7f573da7a9835d42ef085b46914c
Subak Systems <subak@m1mini.local>
2021-06-10 16:01:59 +0900

    2021年 6月10日 木曜日 16時01分59秒 JST

--- state/step/state.ts
+++ state/step/state.ts
@@ -7,31 +7,32 @@ export type StepState = {
 
 export namespace StepState {
   export const initialState = (): StepState =>
-    ({
-      start: false,
-      finish: false,
-      doing: false,
-      done: false
-    })
+  ({
+    start: false,
+    finish: false,
+    doing: false,
+    done: false
+  })
 }
 
-export const startStep = <T extends string>(p: T) =>
+export const startStep = <T extends string> (p: T) =>
   [
     {step: {[p]: {start: true, doing: true}}},
     {step: {[p]: {start: false}}}
   ] as {step: {[P in T]: {start: boolean}}}[]
 
-export const finishStep = <T extends string>(p: T) =>
+export const finishStep = <T extends string> (p: T) =>
   [
     {step: {[p]: {finish: true, doing: false}}},
     {step: {[p]: {finish: false}}}
   ] as {step: {[P in T]: {finish: boolean}}}[]
 
 const createIsActionStep = (action: string) =>
-  <T extends string>(p: T) =>
-    <U extends {step?: {[P in T]?: any } | null}>([state]: U[]) => {
-      if (!state.step) { return false; }
-      if ( !(p in state.step) ) { return false }
+  <T extends string> (p: T) =>
+    <U extends {step?: {[P in T]?: any} | null}> ([state]: U[]) => {
+      if (!state) { return false }
+      if (!state.step) {return false;}
+      if (!(p in state.step)) {return false}
       return (state.step[p] as any)[action] === true;
     }
 
