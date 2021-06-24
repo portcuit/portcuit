dbf7bde28e7df2e3a0e7f66ce3166b917ff3bbdb
Subak Systems <subak@m1mini.local>
2021-06-24 16:24:22 +0900

    2021年 6月24日 木曜日 16時24分22秒 JST

--- spa/server/bff/index.ts
+++ spa/server/bff/index.ts
@@ -1,6 +1,7 @@
-import {directProc, mergeMapProc, sink, Socket, source, Container} from "@pkit/core";
+import {filter, switchMap, takeUntil, toArray} from 'rxjs/operators'
+import {directProc, mergeMapProc, sink, Socket, source, Container, mapProc, SocketData} from "@pkit/core";
 import {SpaState} from '@pkit/spa'
-import {UpdateBatch, startStep} from "@pkit/state";
+import {UpdateBatch, startStep, isStartStep, finishStep, StateData, isNotCompleteStep, isDoingStep} from "@pkit/state";
 import {SpaServerPort} from "../index/";
 
 export class SpaServerBffPort<T extends SpaState> extends SpaServerPort<T> {
@@ -16,6 +17,24 @@ export class SpaServerBffPort<T extends SpaState> extends SpaServerPort<T> {
         }
         return [...batch, startStep('bff')]
       }, sink(port.err))
+
+  finishBffFlow = (port: this) =>
+    mapProc(source(port.state.data).pipe(
+      filter(([, batch]) =>
+        batch.some(isStartStep('bff'))),
+      switchMap(() =>
+        source(port.state.update).pipe(
+          takeUntil(source(port.state.data).pipe(
+            filter(([state]) => port.bffBufferFilter(state)))),
+          toArray()))),
+      sink<UpdateBatch<SpaState>>(port.bffState.update),
+      (batches) => [
+        ...batches.flatMap((batch) => batch),
+        finishStep('bff')
+      ])
+
+  bffBufferFilter = (state: T) =>
+    [state].some(isDoingStep('bff'))
 
   updateBatchFlow = (port: this) =>
     directProc(source(port.bffState.update), sink(port.rest.response.json))
