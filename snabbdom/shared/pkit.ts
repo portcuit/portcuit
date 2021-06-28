db4030f6ca273772ae93d198557c0329c44eb054
Subak Systems <subak@m1mini.local>
2021-06-28 13:24:36 +0900

    2021年 6月28日 月曜日 13時24分36秒 JST

--- snabbdom/shared/pkit.ts
+++ snabbdom/shared/pkit.ts
@@ -6,6 +6,8 @@ export type FC<T extends VNodeData = VNodeData> =
 export const Fragment: FC = (props, children) =>
   children!
 
+export type IProps<T, U> = {state: T, _state?: T, params: U, _params?: U, props?: IProps<T, U>}
+
 export default {
   jsxFactory: jsx,
   jsxFragmentFactory: Fragment
