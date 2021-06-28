55b5abc885ff925b8ffc15fca6d104b2b0306163
Subak Systems <subak@m1mini.local>
2021-06-28 22:08:09 +0900

    2021年 6月28日 月曜日 22時08分09秒 JST

--- spa/shared/state.ts
+++ spa/shared/state.ts
@@ -2,21 +2,23 @@ import {StepState} from "@pkit/state";
 
 export type SpaState = {
   step: {
-    init: StepState;
-    bff: StepState;
-    render: StepState;
-    hydration: StepState;
+    init: StepState
+    bff: StepState
+    render: StepState
+    hydration: StepState
+    err: StepState<Error>
   }
 }
 
 export namespace SpaState {
   export const initialState = (): SpaState =>
-    ({
-      step: {
-        init: StepState.initialState(),
-        bff: StepState.initialState(),
-        render: StepState.initialState(),
-        hydration: StepState.initialState()
-      }
-    })
+  ({
+    step: {
+      init: StepState.initialState(),
+      bff: StepState.initialState(),
+      render: StepState.initialState(),
+      hydration: StepState.initialState(),
+      err: StepState.initialState()
+    }
+  })
 }
